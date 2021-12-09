7ccb76d8b95885656b3e9517ff32b6c137373938
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-12-09 13:22:18 -0300

    Separado os testes de Create dos testes de Update.

---
+++ src/test/java/br/org/enascimento/assembleiacooperados/write/adapter/in/CreatePautaIT.java
@@ -0,0 +1,57 @@
+package br.org.enascimento.assembleiacooperados.write.adapter.in;
+
+import helper.IntegrationHelper;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(properties = "test.dataset=CreatePautaIT")
+class CreatePautaIT extends IntegrationHelper {
+
+    @Test
+    void GIVEN_ValidData_MUST_Created() throws Exception {
+        //given
+        var uuid = randomUUID().toString();
+        var titulo = faker.team().sport();
+        var descricao = faker.lorem().characters(10);
+
+        var payload =
+                """
+                   {
+                      "uuid": "%s",
+                      "titulo": "%s",
+                      "descricao": "%s"
+                   }
+                """.formatted(uuid, titulo, descricao);
+        //when
+        mockMvc
+                .perform(post("/v1/pautas/", uuid)
+                        .contentType(APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(status().isCreated());
+
+        //then
+        mockMvc
+                .perform(get("/v1/pautas/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(exists(uuid))
+                .andExpect(exists(titulo))
+                .andExpect(exists(descricao));
+    }
+
+    public ResultMatcher exists(final String expectedFieldValue) {
+        return mvcResult -> {
+            String json = mvcResult.getResponse().getContentAsString();
+            assertThat(json.contains(expectedFieldValue)).isTrue();
+        };
+    }
+}
---
+++ src/test/java/br/org/enascimento/assembleiacooperados/write/adapter/in/UpdatePautaIT.java
@@ -0,0 +1,40 @@
+package br.org.enascimento.assembleiacooperados.write.adapter.in;
+
+import helper.IntegrationHelper;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+
+import static java.util.UUID.randomUUID;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(properties = "test.dataset=UpdatePautaIT")
+public class UpdatePautaIT extends IntegrationHelper {
+
+
+    @Test
+    void GIVEN_InvalidData_MUST_ReturError() throws Exception {
+        //given
+        var uuid = randomUUID().toString();
+        var titulo = faker.lorem().characters();
+        var descricao = faker.lorem().characters();
+
+        var payload =
+                """
+                   {
+                      "uuid": "%s",
+                      "titulo": "%s",
+                      "descricao": "%s"
+                   }
+                """.formatted(uuid, titulo, descricao);
+        //when
+        mockMvc
+                .perform(post("/v1/pautas/", uuid)
+                        .contentType(APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(status().isBadRequest());
+
+    }
+
+}
