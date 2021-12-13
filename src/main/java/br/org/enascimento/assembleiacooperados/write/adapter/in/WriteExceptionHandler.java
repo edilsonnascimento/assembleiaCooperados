49458afb634fc24321367f90097b68eb4df5aac7
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-12-13 05:42:59 -0300

    fix voltado classe para rodar como um advice.

--- src/main/java/br/org/enascimento/assembleiacooperados/write/adapter/in/WriteExceptionHandler.java
+++ src/main/java/br/org/enascimento/assembleiacooperados/write/adapter/in/WriteExceptionHandler.java
@@ -3,12 +3,11 @@ package br.org.enascimento.assembleiacooperados.write.adapter.in;
 import br.org.enascimento.assembleiacooperados.write.domain.exception.DomainException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,19 +17,10 @@ import java.util.stream.Collectors;
 import static java.lang.String.valueOf;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@RestController
+@RestControllerAdvice
 public class WriteExceptionHandler {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Object> onMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-
-        var errors = List
-                .of(new FieldValidationError(exception.getName(), exception.getValue().toString()));
-
-        return getResponseEntity(exception.getMessage(), errors, BAD_REQUEST);
-    }
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> onMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
@@ -38,12 +28,11 @@ public class WriteExceptionHandler {
         List<FieldValidationError> mapErrors = exception
                 .getBindingResult()
                 .getFieldErrors()
-                .stream().map(fieldError -> {
-                    return new FieldValidationError(fieldError.getField(), fieldError.getDefaultMessage());
-                })
+                .stream()
+                .map(fieldError -> new FieldValidationError(fieldError.getField(), fieldError.getDefaultMessage()))
                 .collect(Collectors.toList());
 
-        return new ResponseEntity<>(mapErrors, BAD_REQUEST);
+        return getResponseEntity("invalid data", mapErrors);
     }
 
     @ExceptionHandler(DomainException.class)
@@ -51,10 +40,10 @@ public class WriteExceptionHandler {
 
         var errors = List.of(new FieldValidationError("code", valueOf(exception.getCode())));
 
-        return getResponseEntity(exception.getMessage(), errors, BAD_REQUEST);
+        return getResponseEntity(exception.getMessage(), errors);
     }
 
-    private ResponseEntity<Object> getResponseEntity(String message, List<FieldValidationError> detailedErrors, HttpStatus status) {
+    private ResponseEntity<Object> getResponseEntity(String message, List<FieldValidationError> detailedErrors) {
 
         Map<String, Object> errorResult = new HashMap<>(Map.of("message", message));
 
@@ -64,27 +53,10 @@ public class WriteExceptionHandler {
         if (logger.isWarnEnabled())
             logger.warn(errorResult.toString());
 
-        return new ResponseEntity<>(errorResult, status);
+        return new ResponseEntity<>(errorResult, BAD_REQUEST);
     }
 
-    static class FieldValidationError {
-
-        private final String field;
-        private final String detail;
-
-        public FieldValidationError(String field, String detail) {
-            this.field = field;
-            this.detail = detail;
-        }
-
-        public String getField() {
-            return field;
-        }
-
-        public String getDetail() {
-            return detail;
-        }
-
+    record FieldValidationError(String field, String detail) {
         @Override
         public String toString() {
             return "field='" + field + '\'' +
