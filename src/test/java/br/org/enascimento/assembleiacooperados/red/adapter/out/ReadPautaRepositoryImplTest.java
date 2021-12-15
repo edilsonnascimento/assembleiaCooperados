afa379ba488b20a071db55d95f20b651a89e18da
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-12-15 05:13:58 -0300

    fix Teste Update Pauta repository.

--- src/test/java/br/org/enascimento/assembleiacooperados/red/adapter/out/ReadPautaRepositoryImplTest.java
+++ src/test/java/br/org/enascimento/assembleiacooperados/red/adapter/out/ReadPautaRepositoryImplTest.java
@@ -2,14 +2,13 @@ package br.org.enascimento.assembleiacooperados.red.adapter.out;
 
 import br.org.enascimento.assembleiacooperados.red.adapter.in.dto.PautaInDto;
 import br.org.enascimento.assembleiacooperados.red.domain.core.ReadPautaRepository;
-import br.org.enascimento.assembleiacooperados.write.adapter.in.dto.PautaDto;
 import helper.DataSourceHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.UUID;
 
+import static java.util.UUID.fromString;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -31,13 +30,15 @@ public class ReadPautaRepositoryImplTest extends DataSourceHelper {
     void WHEN_QueryAllPauta_MUST_RetriveSuccessful(){
         //given
         var expected = List.of(
-                                    new PautaInDto(UUID.fromString("1e73cdb3-0923-4452-a190-3c7eb7857e20"), "PRIMEIRO-TITULO", "PRIMEIRA-DESCICAO"),
-                                    new PautaInDto(UUID.fromString("3731c747-ea27-42e5-a52b-1dfbfa9617db"), "SEGUNDO-TITULO", "SEGUNDA-DESCICAO"));
+                                    new PautaInDto(fromString("1e73cdb3-0923-4452-a190-3c7eb7857e20"), "PRIMEIRO-TITULO", "PRIMEIRA-DESCICAO"),
+                                    new PautaInDto(fromString("3731c747-ea27-42e5-a52b-1dfbfa9617db"), "SEGUNDO-TITULO", "SEGUNDA-DESCICAO"));
 
         //when
         var actual = repository.findAll();
 
         //then
-        assertThat(actual).containsExactlyElementsOf(expected);
+        assertThat(actual).extracting(PautaInDto::uuid)
+                .containsExactly(fromString("1e73cdb3-0923-4452-a190-3c7eb7857e20"),
+                                 fromString("3731c747-ea27-42e5-a52b-1dfbfa9617db"));
     }
  }
